d25785ec5e26ae1fb8c0c50056fbf2e8c4509550
then1601 <truongquangthang1601@gmail.com>
2025-12-03 23:09:35 +0700

    fix1Add project files.

---
+++ WebRazorpage/Pages/Shared/_Message.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazorpage.Pages.Shared
+{
+    public class _MessageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebRazorpage/Pages/Shared/_ProductItem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebRazorpage.Pages.Shared
+{
+    public class _ProductItemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
